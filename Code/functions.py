8a6cd5aa4bb7f83a9d3890163e5277b6b712917b
gerardogonz <gera.gonzf@gmail.com>
2023-02-07 21:17:42 -0600

    Creación de functions.py
    se desarrollo la función para sacar los precios

---
+++ Code/functions.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import yfinance as yf
+
+def precios(tickers,start_date, end_date, fechas_consulta):
+    matriz = yf.download(tickers, start = start_date, end = end_date)['Close'] 
+    serie_MXN = yf.download("MXN=X",start = start_date,end = end_date)["Close"]
+    MXN = pd.DataFrame(data = serie_MXN)
+    mxn_consulta = np.zeros(len(fechas_consulta))
+    matriz_sal = []
+    for i in range(len(fechas_consulta)):
+        mxn_consulta[i] = MXN.loc[fechas_consulta[i]]
+        matriz_sal.append(matriz.loc[fechas_consulta[i]])
+    output = pd.DataFrame(data = matriz_sal, index = fechas_consulta)
+    output["MXN"] = mxn_consulta   
+    return output
