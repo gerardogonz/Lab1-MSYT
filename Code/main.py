0149ea03b76f82b3c1685008a9d92fe8ac2e558c
anadurrmuller <70038176+anadurrmuller@users.noreply.github.com>
2023-02-08 00:01:34 -0600

    correccion

--- Code/main.py
+++ Code/main.py
@@ -1,10 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Feb  7 22:28:16 2023
 
-@author: ana
-"""
 def pasiva_inicial(precios, precios_cash, tickers, pond, cash_pond, c_0):
     # Cálculo de inversión de acuerdo a ponderación y redondeo
     precios_0 = np.array(precios.iloc[0,:])
